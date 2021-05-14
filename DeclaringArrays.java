9657ca38fd6a7a0311684f5e8a85d57070e21ed1
Emma Bradford <ebradford@Emmas-MacBook-Air.local>
2021-05-14 08:57:29 -0500

    finished project

---
+++ DeclaringArrays.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class DeclaringArrays here.
+ *
+ * @author Emma Bradford
+ * @version Jan 8
+ */
+public class DeclaringArrays
+{
+    public static void main(String[] args)
+    {
+        int [] abc = new int[500];
+        
+        int[] def;
+        def = new int[500];
+        
+        int[] ghi, jkl;
+        ghi = new int[400];
+        jkl = new int[1000];
+        
+        int aaa[];
+        
+        int bbb[], ccc, ddd[];
+        
+        int [] eee, fff;
+        int ggg;
+        
+    }
+}
---
+++ ListArray.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class ListArray here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ListArray
+{
+    public static void main (String[] args)
+    {
+        int[] list = {1, 2, 3, 4, 5};
+    
+        System.out.print("This is the list forward");
+        for (int i = 0; i< list.length; i++)
+        {
+            System.out.print(list[i] + " s");
+        }
+    }
+}
